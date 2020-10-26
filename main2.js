d1a00c693b90fdfd6c9ee0497142be8270110bd9
Aymeric <aymeric@macbook-pro-de-aymeric.home>
2020-10-26 14:26:43 +0100

    Add main2.js

--- main2.js
+++ main2.js
@@ -5,14 +5,12 @@ let regex = /.*=.*/gm
 let found = fileContent.match(regex)
 let configFile = process.argv[2] + ".json"
 
-// CREATE A JSON FILE
 fs.open(configFile, 'w+', function (err, f) {
     if (err) {
         return console.error(err);
     }
 })
 
-// CONVERT JSON FORMAT
 fs.writeFile(configFile, JSON.stringify(found, null, '\t'), (err) => {
     if (err) throw err;
 })
